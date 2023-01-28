1261c5f0fe381de59498cd6883b121a2ff9a3a0a
Nelson Cheng <nelsoncheng103@gmail.com>
2023-01-27 19:57:54 -0500

    looping range assignment

---
+++ looping-range.js
@@ -0,0 +1,10 @@
+function range(start, end, step) {
+  const array = [];
+  for (let i = start; i <= end; i += step) {
+    array.push(i);
+  }
+  return array;
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
